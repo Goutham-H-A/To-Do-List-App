67aca752a1814094b18ba0ead4129e6027a8dd6b
user <gouthamubaley15@gmail.com>
2024-01-21 18:39:03 +0530

    dowloaded all the required models

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/',function(req,res){
+    var today = new Date();
+    var currentDay= today.getDay();
+    if(currentDay==6 || currentDay==0){
+        res.send("<h1>Its a weekEnd</h1>");
+    }else{
+        res.send("<h1>Its a weekDay<h1>");
+    }
+});
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
+});
