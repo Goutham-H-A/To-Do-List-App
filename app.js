f31e6e8e6023b2aed03357829d5c04e90b468fc6
user <gouthamubaley15@gmail.com>
2024-01-22 16:27:03 +0530

    Added Item list using array and passing through ejs file

--- app.js
+++ app.js
@@ -3,46 +3,30 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set("view engine","ejs");
+var items =['Wakeup','Drink water','Do Exercise']; // array of list items
 
-app.get('/',function(req,res){
-    var today = new Date();
-    var currentDay= today.getDay();
-    var day="";
+app.set("view engine","ejs"); //To run ejs
 
-    switch (currentDay) {
-        case 0:
-            day="Sunday"
-            break;
-        case 1:
-            day="Monday"
-            break;
-        case 2:
-            day="Tuesday"
-            break;
-        case 3:
-            day="Wednesday"
-            break;
-        case 4:
-            day="Thursday"
-            break;
-        case 5:
-            day="Friday"
-            break;
-        case 6:
-            day="Saturday"
-            break;
-        case 7:
-            day="Sunday"
-            break;
-            
-        default:
-            console.log("Error in getting days"+currentDay);
-            break;
-    }
-    res.render("list",{kindDay:day});
-});
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 });
+
+app.get("/",function(req,res){
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
+    var today  = new Date();
+    
+    var day= today.toLocaleDateString("en-US",options); //javascript method to get date and format
+
+    res.render("list",{kindOfDay:day,newItems:items}); // passing the day to list.ejs 
+    
+});
+
+app.post("/",function(req,res){
+    var item =req.body.newItem;
+    items.push(item); //pushing the new item to items array
+    
+    res.redirect("/");
+});
+
