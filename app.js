5c718e98e4a1fae18d71db01756cf91dba5ddf03
user <gouthamubaley15@gmail.com>
2024-01-22 17:10:14 +0530

    Added CSS

--- app.js
+++ app.js
@@ -3,28 +3,30 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items =['Wakeup','Drink water','Do Exercise']; // array of list items
+let items =['Wakeup','Drink water','Do Exercise']; // array of list items
 
 app.set("view engine","ejs"); //To run ejs
 
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public")); //setting static files public to use all files like css ect..
 
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 });
 
 app.get("/",function(req,res){
-    var options = { weekday: 'long', month: 'long', day: 'numeric' };
-    var today  = new Date();
+    let options = { weekday: 'long', month: 'long', day: 'numeric' };
+    let today  = new Date();
     
-    var day= today.toLocaleDateString("en-US",options); //javascript method to get date and format
+    let day= today.toLocaleDateString("en-US",options); //javascript method to get date and format
 
     res.render("list",{kindOfDay:day,newItems:items}); // passing the day to list.ejs 
     
 });
 
 app.post("/",function(req,res){
-    var item =req.body.newItem;
+    let item =req.body.newItem;
     items.push(item); //pushing the new item to items array
     
     res.redirect("/");
